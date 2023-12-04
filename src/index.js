8c9b4d3685b28ca17ef8cfb003e9af61ea6c9922
Estercade <withrwithoutyou@gmail.com>
2023-12-04 12:23:05 -0500

    Add functionality to fetch weather data
    Add search by zip code functionality and basic search box

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 import "./stylesheet.css";
 
-console.log("hello world");
+const appid = '8c6756b80aabdd4de605502eeb301ee9';
+
+async function getWeatherByCoords(lat, lon, appid) {
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}`, {
+        mode: 'cors'
+    });
+
+    let weatherJSON = await response.json();
+    console.log(weatherJSON);
+}
+
+// getWeatherByCoords('40.7128', '-74.0060', appid);
+
+const searchWrapper = document.getElementById('search-wrapper');
+
+const searchbox = document.createElement('input');
+searchbox.type = 'text';
+searchbox.id = 'searchbox';
+searchWrapper.append(searchbox);
+
+const searchBtn = document.createElement('button');
+searchBtn.type = 'button';
+searchBtn.innerText = 'Submit';
+searchWrapper.append(searchBtn);
+
+async function getWeatherByZip(zip, country, appid) {
+    try {
+        let locationData = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${zip},${country}&appid=${appid}`, {
+            mode: 'cors'
+        });
+
+        let locationJSON = await locationData.json();
+        console.log(locationJSON);
+
+        getWeatherByCoords(await locationJSON.lat, await locationJSON.lon, appid);
+
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
+getWeatherByZip('10280', 'US', appid);
