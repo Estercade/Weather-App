3b911438a8d5f508c8c2394477c0e4abd26966c4
Estercade <withrwithoutyou@gmail.com>
2023-12-07 14:43:00 -0500

    Separate weather display function into components

--- src/index.js
+++ src/index.js
@@ -54,48 +54,85 @@ function displayWeather(weatherJSON, units) {
         windSpeedUnits = 'mph';
     }
 
+    displayDateTime(weatherJSON, currentWeatherWrapper);
+    displayMainWeatherData(weatherJSON, currentWeatherWrapper, tempUnits)
+    displayExtraWeatherData(weatherJSON, currentWeatherWrapper, tempUnits, windSpeedUnits)
+}
+
+function displayDateTime(weatherJSON, currentWeatherWrapper) {
     let unixDT = weatherJSON.dt;
     let [formattedDate, formattedTime] = convertDT(unixDT);
     
+    const datetimeWrapper = document.createElement('div');
+
     const currentDate = document.createElement('div');
     currentDate.innerText = formattedDate;
-    currentWeatherWrapper.appendChild(currentDate); 
+    datetimeWrapper.appendChild(currentDate); 
 
     const currentTime = document.createElement('div');
     currentTime.innerText = formattedTime;
-    currentWeatherWrapper.appendChild(currentTime); 
+    datetimeWrapper.appendChild(currentTime); 
+
+    currentWeatherWrapper.appendChild(datetimeWrapper);
+}
+
+function displayMainWeatherData(weatherJSON, currentWeatherWrapper, tempUnits) {
+    const mainWeatherDataWrapper = document.createElement('div');
 
     const currentLocation = document.createElement('div');
     currentLocation.innerText = weatherJSON.name;
-    currentWeatherWrapper.appendChild(currentLocation);
+    mainWeatherDataWrapper.appendChild(currentLocation);
 
     const currentWeatherConditions = document.createElement('div');
     currentWeatherConditions.innerText = weatherJSON.weather[0].description;
-    currentWeatherWrapper.appendChild(currentWeatherConditions);
+    mainWeatherDataWrapper.appendChild(currentWeatherConditions);
 
     const currentTemp = document.createElement('div');
     currentTemp.innerHTML = `${weatherJSON.main.temp} ${tempUnits}`;
-    currentWeatherWrapper.appendChild(currentTemp);
+    mainWeatherDataWrapper.appendChild(currentTemp);
 
     const currentTempMax = document.createElement('div');
     currentTempMax.innerHTML = `H: ${weatherJSON.main.temp_max} ${tempUnits}`;
-    currentWeatherWrapper.appendChild(currentTempMax);
+    mainWeatherDataWrapper.appendChild(currentTempMax);
 
     const currentTempMin = document.createElement('div');
     currentTempMin.innerHTML = `L: ${weatherJSON.main.temp_min} ${tempUnits}`;
-    currentWeatherWrapper.appendChild(currentTempMin);
+    mainWeatherDataWrapper.appendChild(currentTempMin);
 
-    const currentTempFeelsLike = document.createElement('div');
-    currentTempFeelsLike.innerHTML = `Feels like: ${weatherJSON.main.feels_like} ${tempUnits}`;
-    currentWeatherWrapper.appendChild(currentTempFeelsLike);
+    currentWeatherWrapper.appendChild(mainWeatherDataWrapper);
+}
 
-    const currentHumidity = document.createElement('div');
-    currentHumidity.innerText = `Humidity: ${weatherJSON.main.humidity}%`;
-    currentWeatherWrapper.appendChild(currentHumidity);
+function displayExtraWeatherData(weatherJSON, currentWeatherWrapper, tempUnits, windSpeedUnits) {
+    const extraWeatherDataWrapper = document.createElement('ul');
 
-    const currentWind = document.createElement('div');
-    currentWind.innerText = `Wind speed: ${weatherJSON.wind.speed} ${windSpeedUnits}`;
-    currentWeatherWrapper.appendChild(currentWind);
+    const currentTempFeelsLike = document.createElement('li');
+    const currentTempFeelsLikeTitle = document.createElement('h4');
+    currentTempFeelsLikeTitle.innerText = `Feels like:`;
+    const currentTempFeelsLikeData = document.createElement('p');
+    currentTempFeelsLikeData.innerHTML = `${weatherJSON.main.feels_like} ${tempUnits}`;
+    currentTempFeelsLike.appendChild(currentTempFeelsLikeTitle);
+    currentTempFeelsLike.appendChild(currentTempFeelsLikeData);
+    extraWeatherDataWrapper.appendChild(currentTempFeelsLike);
+
+    const currentHumidity = document.createElement('li');
+    const currentHumidityTitle = document.createElement('h4');
+    currentHumidityTitle.innerText = `Humidity:`;
+    const currentHumidityData = document.createElement('p');
+    currentHumidityData.innerHTML = `${weatherJSON.main.humidity}%`;
+    currentHumidity.appendChild(currentHumidityTitle);
+    currentHumidity.appendChild(currentHumidityData);
+    extraWeatherDataWrapper.appendChild(currentHumidity);
+
+    const currentWind = document.createElement('li');
+    const currentWindTitle = document.createElement('h4');
+    currentWindTitle.innerText = `Wind speed:`;
+    const currentWindData = document.createElement('p');
+    currentWindData.innerHTML = `${weatherJSON.wind.speed} ${windSpeedUnits}`;
+    currentWind.appendChild(currentWindTitle);
+    currentWind.appendChild(currentWindData);
+    extraWeatherDataWrapper.appendChild(currentWind);
+
+    currentWeatherWrapper.appendChild(extraWeatherDataWrapper);
 }
 
 getWeatherByZip('10280', 'US', 'metric', appid);
